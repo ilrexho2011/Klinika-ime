9333f199cc18ad94e6792b4910d1763c436a1c1a
Ilirjan Rexho <61479363+ilrexho2011@users.noreply.github.com>
2022-07-10 23:36:34 +0200

    Add files via upload

---
+++ sistemi/language/english/calendar_lang.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * CodeIgniter
+ *
+ * An open source application development framework for PHP
+ *
+ * This content is released under the MIT License (MIT)
+ *
+ * Copyright (c) 2014 - 2015, British Columbia Institute of Technology
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @package	CodeIgniter
+ * @author	EllisLab Dev Team
+ * @copyright	Copyright (c) 2008 - 2014, EllisLab, Inc. (http://ellislab.com/)
+ * @copyright	Copyright (c) 2014 - 2015, British Columbia Institute of Technology (http://bcit.ca/)
+ * @license	http://opensource.org/licenses/MIT	MIT License
+ * @link	http://codeigniter.com
+ * @since	Version 1.0.0
+ * @filesource
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['cal_su'] = 'Di';
+$lang['cal_mo'] = 'He';
+$lang['cal_tu'] = 'Ma';
+$lang['cal_we'] = 'Me';
+$lang['cal_th'] = 'En';
+$lang['cal_fr'] = 'Pr';
+$lang['cal_sa'] = 'Sh';
+$lang['cal_sun'] = 'Die';
+$lang['cal_mon'] = 'Hen';
+$lang['cal_tue'] = 'Tue';
+$lang['cal_wed'] = 'Wed';
+$lang['cal_thu'] = 'Thu';
+$lang['cal_fri'] = 'Fri';
+$lang['cal_sat'] = 'Sat';
+$lang['cal_sunday'] = 'Sunday';
+$lang['cal_monday'] = 'Monday';
+$lang['cal_tuesday'] = 'Tuesday';
+$lang['cal_wednesday'] = 'Wednesday';
+$lang['cal_thursday'] = 'Thursday';
+$lang['cal_friday'] = 'Friday';
+$lang['cal_saturday'] = 'Saturday';
+$lang['cal_jan'] = 'Jan';
+$lang['cal_feb'] = 'Feb';
+$lang['cal_mar'] = 'Mar';
+$lang['cal_apr'] = 'Pri';
+$lang['cal_may'] = 'Maj';
+$lang['cal_jun'] = 'Qer';
+$lang['cal_jul'] = 'Kor';
+$lang['cal_aug'] = 'Gus';
+$lang['cal_sep'] = 'Sht';
+$lang['cal_oct'] = 'Tet';
+$lang['cal_nov'] = 'Nen';
+$lang['cal_dec'] = 'Dhj';
+$lang['cal_january'] = 'Janar';
+$lang['cal_february'] = 'Shkurt';
+$lang['cal_march'] = 'Mars';
+$lang['cal_april'] = 'Prill';
+$lang['cal_mayl'] = 'Maj';
+$lang['cal_june'] = 'Qershor';
+$lang['cal_july'] = 'Korrik';
+$lang['cal_august'] = 'Gusht';
+$lang['cal_september'] = 'Shtator';
+$lang['cal_october'] = 'Tetor';
+$lang['cal_november'] = 'Nentor';
+$lang['cal_december'] = 'Dhjetor';
